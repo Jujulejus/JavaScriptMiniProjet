51079532b1bbce2b5308a27b030fb562fd4eba9d
Miss-Simra <simraketheeswaran@gmail.com>
2024-10-26 16:58:28 +0200

    MAJ

--- projet-cuisine/script.js
+++ projet-cuisine/script.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
       // Récupère l'image de l'ingrédient à l'aide du texte saisi
 
       $("#ingredient-image").html(
-        `<img src="${ingredientImages}" alt="${ingredientInput} onerror="<div>Ingredient not found.</div>">`
+        `<img src="${ingredientImages}" alt="${ingredientInput}>`
       );
 
       $.ajax({
