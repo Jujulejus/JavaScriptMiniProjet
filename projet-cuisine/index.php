3cd9143f239a84033ef443327f2de8ad0844eee4
Jujulejus <juliencompare@gmail.com>
2024-11-09 16:09:36 +0100

    Amélioration Visuel du site

--- projet-cuisine/index.php
+++ projet-cuisine/index.php
@@ -9,23 +9,35 @@
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <div class="container mt-5">
-        <h1 class="text-center">ThemealDB API</h1>
-        <form id="ingredient-form" class="form-inline position-relative">
-            <div class="form-group mb-2">
-                <label for="ingredient-input" class="sr-only">Ingrédient</label>
-                <input type="text" id="ingredient-input" class="form-control" placeholder="Enter an ingredient" required autocomplete="off">
-                <div id="suggestions" class="suggestions"></div>
+    <header>
+        <div class="realbody">
+            <div class="LogoName">
+                <h1 class="titlesite">UltraRecipe<img class="logodusite" src="./RecetteSiteLogo.png" alt="logodusite"></img></h1>
             </div>
-            <button type="submit" class="btn btn-primary mb-2 ml-2">Search</button>
-        </form>
-        <button id="random-recipe" class="btn btn-secondary mb-3">Recette Aléatoire</button>
-        <div id="ingredient-image" class="ingredient-image mb-3"></div>
-        <div id="recipe-list" class="recipes"></div>
-        <div id="recipe-details" class="mt-4"></div>
+        </header>
+        <div class="container mt-5">
+            <h1 class="text-center">Réalisé grâce à L'API ThemealDB</h1>
+            <form id="ingredient-form" class="form-inline position-relative">
+                <div class="form-group mb-2">
+                    <label for="ingredient-input" class="sr-only">Ingrédient</label>
+                    <input type="text" id="ingredient-input" class="form-control" placeholder="Enter an ingredient" required autocomplete="off">
+                    <div id="suggestions" class="suggestions"></div>
+                </div>
+                <button type="submit" class="btn btn-primary mb-2 ml-2">Search</button>
+            </form>
+            <button id="random-recipe" class="btn btn-secondary mb-3">Recette Aléatoire</button>
+            <div id="ingredient-image" class="ingredient-image mb-3"></div>
+            <div id="recipe-list" class="recipes"></div>
+            <div id="recipe-details" class="mt-4"></div>
+        </div>
     </div>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="script.js"></script>
 </body>
+
+<footer>
+    <a>FIN</a>
+</footer>
+
 </html>
